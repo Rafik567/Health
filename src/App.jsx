42d62a62b92912e8f13288c50d7f3c119f5ff17a
Admin <admin@192.168.1.207>
2025-03-27 23:00:55 +0400

    add Teask

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import Tesak from './Components/Tesak/Tesak'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+      <Header />
+      <div className='div1'>
+        <Tesak />
+      </div>
     </>
   )
 }
---
+++ src/Components/Tesak/Tesak.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './style.css'
+const Tesak = () => {
+    return (
+        <div class="cont4">
+            <div class="fon-1">
+                <div class="slider-info">
+                    <h1 class="text">Անհատական</h1>
+                    <p class="text">Բալանսավորված սննդակարգ</p>
+                </div>
+                <div class="button-1">
+                    <button class="butt">ՄԶՑ հաշվիչ</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Tesak
