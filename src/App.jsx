e3bf8dd6d535a296473c8c6f12ee12bfca09fa63
Admin <admin@192.168.1.207>
2025-03-27 23:06:12 +0400

    add Advanture

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
 import Tesak from './Components/Tesak/Tesak'
+import Advanture from './Components/Advanture/Advanture'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <div className='div1'>
         <Tesak />
       </div>
+      <Advanture/>
     </>
   )
 }
